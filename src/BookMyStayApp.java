8e6494156d121bdbd3db293245e817190900f444
abhixn9 <abhixin79@gmail.com>
2026-03-12 09:43:04 +0530

    UC4:Room search and avaliability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,23 +1,15 @@
-public class UseCase3InventorySetup {
+public class UseCase4RoomSearch {
 
     public static void main(String[] args) {
 
         System.out.println("=================================");
         System.out.println("Book My Stay - Hotel Booking App");
-        System.out.println("Version 3.1");
+        System.out.println("Version 4.1");
         System.out.println("=================================");
 
         RoomInventory inventory = new RoomInventory();
+        RoomSearchService searchService = new RoomSearchService(inventory);
 
-        inventory.displayInventory();
-
-        System.out.println("\nChecking availability...");
-        System.out.println("Single Room Available: "
-                + inventory.getAvailability("Single Room"));
-
-        System.out.println("\nUpdating availability...");
-        inventory.updateAvailability("Single Room", 8);
-
-        inventory.displayInventory();
+        searchService.searchAvailableRooms();
     }
 }
