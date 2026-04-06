9abdda9170fa66658360cf3e40b8d42131ce037d
abhi <abhi@hareeshs-MacBook-Air-1838.local>
2026-04-06 23:19:27 +0530

    UC1:applicationentryandwelcomemessage

--- main.java
+++ main.java
@@ -1,74 +1,11 @@
-// Abstract Room class
-abstract class Room {
-    protected String type;
-    protected int beds;
-    protected double price;
-
-    public Room(String type, int beds, double price) {
-        this.type = type;
-        this.beds = beds;
-        this.price = price;
-    }
-
-    // Common method to display details
-    public void displayDetails() {
-        System.out.println("Room Type: " + type);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: " + price);
-    }
-}
-
-// Single Room class
-class SingleRoom extends Room {
-
-    public SingleRoom() {
-        super("Single Room", 1, 2000);
-    }
-}
-
-// Double Room class
-class DoubleRoom extends Room {
-
-    public DoubleRoom() {
-        super("Double Room", 2, 3500);
-    }
-}
-
-// Suite Room class
-class SuiteRoom extends Room {
-
-    public SuiteRoom() {
-        super("Suite Room", 3, 5000);
-    }
-}
-
-// Main class
-public class UseCase2RoomInitialization {
+public class UseCase1HotelBookingApp {
 
     public static void main(String[] args) {
 
-        // Create room objects (Polymorphism)
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        System.out.println("==================================");
+        System.out.println("   Welcome to Book My Stay App   ");
+        System.out.println("   Hotel Booking System v1.0     ");
+        System.out.println("==================================");
 
-        // Static availability (simple variables)
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 1;
-
-        // Display details
-        System.out.println("Room Details and Availability:\n");
-
-        single.displayDetails();
-        System.out.println("Available: " + singleAvailable);
-        System.out.println("-------------------------");
-
-        doubleRoom.displayDetails();
-        System.out.println("Available: " + doubleAvailable);
-        System.out.println("-------------------------");
-
-        suite.displayDetails();
-        System.out.println("Available: " + suiteAvailable);
     }
 }
